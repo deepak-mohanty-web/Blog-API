291413736c697083ba5c86c0356f76e065f2560a
Deepak Mohanty <mohantydeepak284@gmail.com>
2025-06-07 10:59:20 +0530

    adding Login Api to it

---
+++ src/controllers/v1/auth/login.ts
@@ -0,0 +1,67 @@
+//custom modules
+import config from '@/config';
+import { generateAccessToken, generateRefreshToken } from '@/lib/jwt';
+import { logger } from '@/lib/winston';
+
+// models
+import Token from '@/models/token';
+import User from '@/models/user';
+
+// types
+import type { IUser } from '@/models/user';
+import type { Request, Response } from 'express';
+
+type UserData = Pick<IUser, 'email' | 'password'>
+
+const login = async (req: Request, res: Response):Promise<void> => {
+
+  try {
+    const { email } = req.body as UserData;
+    const user = await User.findOne({ email })
+      .select('username email password role')
+      .lean()
+      .exec();
+    if (!user) {
+      res.status(404).json({
+        code: "NotFound",
+        message: "User not found."
+      });
+      return;
+    }
+
+    // generate access token and refresh for new user
+    const accessToken = generateAccessToken(user._id);
+    const refreshToken = generateRefreshToken(user._id);
+    // store the token to the Database
+    await Token.create({ token: refreshToken, userId: user._id });
+    logger.info('Refresh token created for user', {
+      userId: user._id,
+      token:refreshToken
+    })
+
+    res.cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      secure: config.NODE_ENV === 'production',
+      sameSite:'strict'
+    })
+
+   res.status(201).json({
+     user: {
+       username: user.username,
+       email: user.email,
+       role:user.role
+     },
+     accessToken
+   })
+   
+    logger.info('User register successful',user)
+ } catch (error) {
+   res.status(500).json({
+     code:"ServerError",
+     message: "Internal server Error.",
+     error:error
+   })
+   logger.error('Error during user registration.',error)
+ }
+}
+export default login;
--- src/routes/v1/auth.ts
+++ src/routes/v1/auth.ts
@@ -1,7 +1,9 @@
 // node modules
+import bcrypt from 'bcrypt';
 import { Router } from 'express';
 import { body } from 'express-validator';
 //controllers
+import login from '@/controllers/v1/auth/login';
 import register from '@/controllers/v1/auth/register';
 //middleware
 import validationError from '@/middlewares/validationError';
@@ -43,5 +45,48 @@ router.post('/register',
 
   validationError,
   register);
+router.post(
+  '/login',
+  body('email')
+    .trim()
+    .notEmpty()
+    .withMessage("Email is required")
+    .isLength({ max: 50 })
+    .withMessage("Email must be less than 50 characters")
+    .isEmail()
+    .withMessage("Invalid email address")
+    .custom(async (value) => {
+      const existUser = await User.exists({ email: value });
+      if (!existUser) {
+        throw new Error("User email and password is invalid");
+        
+      }
+    })
+  ,
+  body('password')
+    .notEmpty()
+    .withMessage("Password is required")
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters long")
+    .custom(async (value, { req }) => {
+      const { email } = req.body as { email: string };
+      const user = await User.findOne({ email })
+        .select('password')
+        .lean()
+        .exec();
+      if (!user) {
+        throw new Error('User email and password is invalid')
+      }
+
+      const isMatch = await bcrypt.compare(value, user.password);
+      if (!isMatch) {
+        throw new Error('User email and password is invalid')
+      }
+
+    })
+  ,
+  validationError,
+  login
+);
 
 export default router;
